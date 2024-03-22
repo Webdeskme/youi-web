1cf99e7999f19a92700f1f2a8973c69fa946b669
Webdesk-me <atelford@webdesk.me>
2024-03-22 15:33:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 console.log('Initially ' + (window.navigator.onLine ? 'on' : 'off') + 'line');
 
 window.addEventListener('online', () => console.log('Became online'));
-window.addEventListener('offline', () => console.log('Became offline'); window.location.href = "http://10.42.0.1:3000";);
+window.addEventListener('offline', () => window.location.href = "http://10.42.0.1:3000");
 
 document.getElementById('statusCheck').addEventListener('click', () => console.log('window.navigator.onLine is ' + window.navigator.onLine));
