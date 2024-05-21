c6f3304c3532980ddd3512c29c3eea107790b87e
webdeskme <atelford@webdesk.me>
2024-05-21 12:40:38 -0400

    this is a test

--- screen.js
+++ screen.js
@@ -2,4 +2,5 @@ this.socket = io('http://127.0.0.1:3333');
 this.socket.on('test', function (test) {
         console.log(test);
 	document.getElementById("connect").innerHTML = test;
+	this.socket.emit('bob', "teating123");
 });
