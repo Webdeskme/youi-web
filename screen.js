681f2e1499d610c2d9ba8a87d4f00192a65c4363
webdeskme <atelford@webdesk.me>
2024-05-21 12:52:47 -0400

    fix 1

--- screen.js
+++ screen.js
@@ -2,5 +2,5 @@ this.socket = io('http://127.0.0.1:3333');
 this.socket.on('test', function (test) {
         console.log(test);
 	document.getElementById("connect").innerHTML = test;
-	this.socket.emit('bob', "teating123");
+	socket.emit('bob', "testing123");
 });
